057a1a42d3282e7e646153234cd6448c4a31ac00
poojaa09 <poojarawat0329@gmail.com>
2025-09-08 02:57:42 +0530

    implement interview generation page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Agent from "@/components/Agent";
+const Page=()=>{
+    return (
+        <>
+            <h3>Interview Genertaion</h3>
+<Agent userName="You" userId="user" />
+        </>
+    )
+}
+export default Page
